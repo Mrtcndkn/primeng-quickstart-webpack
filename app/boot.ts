ca84247b9293856acd4f919ce684927c1faab68a
Mrtcndkn <mrtcndkn@gmail.com>
2016-05-23 16:23:55 +0300

    Angular 2 RC 1 update

--- app/boot.ts
+++ app/boot.ts
@@ -1,7 +1,15 @@
 import 'script!primeui/primeui-ng-all.min.js';
-import 'angular2/bundles/angular2-polyfills';
-import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
+import 'reflect-metadata';
+import '@angular/platform-browser';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router-deprecated';
+
 import 'rxjs/Rx';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {AppComponent} from './app.component';
 
 bootstrap(AppComponent);
